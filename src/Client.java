89743a703024014a141b35e754bdabb1632742ef
Mike <msthygesen16@gmail.com>
2021-08-07 17:57:36 +1000

    converts string input to int list

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.ArrayList;
 
 public class Client {
     
@@ -19,7 +20,7 @@ public class Client {
 
         String msg = console.readLine(str);
         return msg;
-        
+
     }
  
     public static void main(String[] args) throws Exception {
@@ -43,36 +44,72 @@ public class Client {
         return num;
     }
 
-    // return an array of Strings that contain the members of a particular group
-    public String[] inputGroupData(int groupSize){
 
-        String[] groupData = new String[groupSize];
+    
+    // given the number of groups by the user
+    // we return an ArrayList of ArrayLists that contain Integers
+    // we populate this list of lists by calling groupLineToList <-- returns an arrayList of integers
+    ArrayList<ArrayList<Integer>> strArrToArrList(int numGroups){
+
+        ArrayList<ArrayList<Integer>> returnList = new ArrayList<ArrayList<Integer>>();
 
         console.printf("Enter the list of members of each group (one group per line, each terminated by 0):\n");
-
-        for (int i = 0; i < groupSize; i++){
-            groupData[i] = userInput("");
+    
+        for (int i = 0; i < numGroups; i++){
+            String tempStr = userInput("");
+            returnList.add(groupLineToList(tempStr));
         }
 
-        return groupData;
+        return returnList;
+    }
+
+    // given a string input this splits up the string into an array of strings
+    // casts all elements of the string array to integers and adds it to an arrayList of integers
+    // return that arrayList of integers
+    public ArrayList<Integer> groupLineToList(String lineIn){ 
+
+        String[] splitStr = lineIn.split("\\s+");
+        ArrayList<Integer> returnList = new ArrayList<Integer>();
+
+        for (int i = 0; i < splitStr.length-1; i ++){ // we do -1 to ommit the 0 at the end
+            returnList.add(i, Integer.parseInt(splitStr[i]));
+        }
+
+        return returnList;
     }
 
     // method that prompts user to give input -- converts it to an Int and returns int value
     public int inputNumberOfGroups(){
-        String tempNum = userInput("Enter the number of groups from which you must find representatives: ");
-        int numGroups = strToInt(tempNum);
+        String strGroupNum = userInput("Enter the number of groups from which you must find representatives: ");
+        int numGroups = strToInt(strGroupNum);
 
         return numGroups;
     }
 
+    // this is where we will call most of our functions
     public void start(){
         
         int numGroups = inputNumberOfGroups();
-        String[] groupData = inputGroupData(numGroups);
+        ArrayList<ArrayList<Integer>> groupDataLists = strArrToArrList(numGroups);
 
-        for (int i = 0; i < numGroups; i++){
-            console.printf(groupData[i] + " ");
+        System.out.println();
+
+        for(ArrayList<Integer> innerList : groupDataLists) {
+
+            int curr = groupDataLists.indexOf(innerList);
+            curr = curr + 1;
+
+            System.out.print("Group: " + curr + " [ ");
+
+            for(Integer number : innerList) {
+                System.out.print(number + " ");
+            }
+
+            System.out.print("]");
+            System.out.println();
         }
+
+        System.out.println();
 
     }
 
