c2036f85e15aac2b0ea8e006b85bc1269bf40ea2
Mike <msthygesen16@gmail.com>
2021-09-02 20:59:56 +1000

    added brief mode

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# for storing what test file
+# for storing what test file & options
+# e.g. specific test case or "-all" or "brief"
 var=$1
 
-# case match any input
+# case match certain input
 case $var in
 
 
@@ -13,6 +14,8 @@ case $var in
 
         # compile java file before running
         javac ass1_comp3010.java
+
+        echo $'\t~~~~~~~~~~~~~~~~~~Beginning Tests~~~~~~~~~~~~~~~~~~'
 
         # for loop; f is file, tests/* is all the files in the folder 'src/tests/'
         for f in ../tests/*; 
@@ -29,7 +32,34 @@ case $var in
         # finish loop and exit the script
 
         # Show we are at end of test set
-        echo $'\t~~~Tests Completed~~~'
+        echo $'\t~~~~~~~~~~~~~~~~~~Tests Completed~~~~~~~~~~~~~~~~~~'
+        ;;
+
+
+    "-brief")
+
+        # change to src folder
+        cd src/
+
+        # compile java file before running
+        javac ass1_comp3010.java
+
+         echo $'\t~~~~~~~~~~~~~~~~~~Beginning Tests~~~~~~~~~~~~~~~~~~'
+
+        # for loop; f is file, tests/* is all the files in the folder 'src/tests/'
+        for f in ../tests/*; 
+            do echo $'\n=============================================';
+            echo $'\t' "Testing file: $f"; 
+            echo $'=============================================';
+            java ass1_comp3010 < $f;     
+            echo $'\n\t [---END OF TEST---] \n';
+        done
+        # the above is some formatting to make the output look nice
+
+        # finish loop and exit the script
+
+        # Show we are at end of test set
+        echo $'\t~~~~~~~~~~~~~~~~~~Tests Completed~~~~~~~~~~~~~~~~~~'
         ;;
 
     *) 
