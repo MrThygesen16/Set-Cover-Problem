49189894ace7d0a6e7d121f427fc5ee58b3f0b2d
Mike <msthygesen16@gmail.com>
2021-09-02 15:24:03 +1000

    added test all at once

--- test.sh
+++ test.sh
@@ -6,6 +6,29 @@ var=$1
 # case match any input
 case $var in
 
+
+    "-all")
+        # change to src folder
+        cd src/
+
+        # compile java file before running
+        javac ass1_comp3010.java
+
+        # for loop; f is file, tests/* is all the files in the folder 'src/tests/'
+        for f in tests/*; 
+            do echo $'\n=============================================';
+            echo $'\t' "Testing file: $f"; 
+            echo $'=============================================';
+            cat $f;
+            echo $'\n';
+            java ass1_comp3010 < $f;     
+            echo $'\n\n \t [---END OF TEST---] \n';
+        done
+        # the above is some formatting to make the output look nice
+
+        # finish loop and exit the script
+        ;;
+
     *) 
         # change to src folder
         cd src/
